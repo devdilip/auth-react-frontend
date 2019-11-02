50d78c37cd8ab105cfcada12164d5b3a3a95c38b
Dilip <dilipkr7587@gmail.com>
2019-11-03 04:38:11 +0530

    Configured Route in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import WrappingComponent from './Components/HigherOrderComponents/WrappingComponent';
+import AppRoutingModule from './Routing';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <WrappingComponent>
+          <BrowserRouter>
+            <AppRoutingModule />
+          </BrowserRouter>
+      </WrappingComponent>
+    );
+  }
 }
 
 export default App;
