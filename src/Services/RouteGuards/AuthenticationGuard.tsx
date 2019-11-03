ee5d9f67a93bd5c8bbbd424ae17a0c393d5a0329
Dilip <dilipkr7587@gmail.com>
2019-11-03 17:03:05 +0530

    Added Route Guard, For Authentication"

---
+++ src/Services/RouteGuards/AuthenticationGuard.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Redirect } from 'react-router-dom';
+
+import { AppLocalStorage, AppStorageKeys } from '../../Contracts';
+import { AppRoute } from '../../Routing';
+import { UserLoginResponse } from '../../Contracts/Login';
+
+export const AuthenticationGuard = (Component) => {
+    return class AuthenticationGuard extends React.Component {
+        render() {
+            let tokenExists = false;
+            const token: UserLoginResponse = AppLocalStorage.get(AppStorageKeys.AppToken, true) as UserLoginResponse;
+            if (token && token.token && token.token.length > 0) {
+                tokenExists = true;
+            }
+            return tokenExists ? <Component {...this.props} /> : <Redirect to={AppRoute.Login} />;
+        }
+    };
+};
